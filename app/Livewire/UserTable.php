eb118c2a82702532ecd25086708ee10b3337c658
AndersonBH16 <ander.bh.16@gmail.com>
2024-08-23 10:03:46 -0500

    Setup Datatable view

--- app/Livewire/UserTable.php
+++ app/Livewire/UserTable.php
@@ -2,17 +2,55 @@
 
 namespace App\Livewire;
 
+use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Columns\{BooleanColumn, ImageColumn};
+use Rappasoft\LaravelLivewireTables\Views\Filters\{DateFilter, MultiSelectFilter, SelectFilter};
 use App\Models\User;
 
 class UserTable extends DataTableComponent
 {
     protected $model = User::class;
 
+    public string $tableName = 'users';
+    public array $users = [];
+
+    public $columnSearch = [
+        'name' => null,
+        'email' => null,
+    ];
+
     public function configure(): void
     {
-        $this->setPrimaryKey('id');
+        $this->setPrimaryKey('id')
+            ->setSingleSortingDisabled()
+            ->setHideReorderColumnUnlessReorderingEnabled()
+            ->setFilterLayoutSlideDown()
+            ->setRememberColumnSelectionDisabled()
+            ->setSecondaryHeaderTrAttributes(function($rows) {
+                return ['class' => 'bg-gray-100'];
+            })
+            ->setSecondaryHeaderTdAttributes(function(Column $column, $rows) {
+                if ($column->isField('id')) {
+                    return ['class' => 'text-red-500'];
+                }
+
+                return ['default' => true];
+            })
+            ->setFooterTrAttributes(function($rows) {
+                return ['class' => 'bg-gray-100'];
+            })
+            ->setFooterTdAttributes(function(Column $column, $rows) {
+                if ($column->isField('name')) {
+                    return ['class' => 'text-green-500'];
+                }
+
+                return ['default' => true];
+            })
+            ->setUseHeaderAsFooterEnabled()
+            ->setHideBulkActionsWhenEmptyEnabled();
+        ;
     }
 
     public function columns(): array
@@ -21,13 +59,101 @@ class UserTable extends DataTableComponent
             Column::make("Id", "id")
                 ->sortable(),
             Column::make("Name", "name")
-                ->sortable(),
+                ->sortable()
+                ->searchable(),
             Column::make("Email", "email")
-                ->sortable(),
+                ->sortable()
+                ->searchable(),
             Column::make("Created at", "created_at")
                 ->sortable(),
             Column::make("Updated at", "updated_at")
                 ->sortable(),
         ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            SelectFilter::make('E-mail Verified', 'email_verified_at')
+                ->setFilterPillTitle('Verified')
+                ->options([
+                    ''    => 'Any',
+                    'yes' => 'Yes',
+                    'no'  => 'No',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    if ($value === 'yes') {
+                        $builder->whereNotNull('email_verified_at');
+                    } elseif ($value === 'no') {
+                        $builder->whereNull('email_verified_at');
+                    }
+                }),
+//            SelectFilter::make('Active')
+//                ->setFilterPillTitle('User Status')
+//                ->setFilterPillValues([
+//                    '1' => 'Active',
+//                    '0' => 'Inactive',
+//                ])
+//                ->options([
+//                    '' => 'All',
+//                    '1' => 'Yes',
+//                    '0' => 'No',
+//                ])
+//                ->filter(function(Builder $builder, string $value) {
+//                    if ($value === '1') {
+//                        $builder->where('active', true);
+//                    } elseif ($value === '0') {
+//                        $builder->where('active', false);
+//                    }
+//                }),
+            DateFilter::make('Verified From')
+                ->config([
+                    'min' => '2020-01-01',
+                    'max' => '2021-12-31',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    $builder->where('email_verified_at', '>=', $value);
+                }),
+            DateFilter::make('Verified To')
+                ->filter(function(Builder $builder, string $value) {
+                    $builder->where('email_verified_at', '<=', $value);
+                }),
+        ];
+    }
+
+    public function builder(): Builder
+    {
+        return User::query()
+            ->when($this->columnSearch['name'] ?? null, fn ($query, $name) => $query->where('users.name', 'like', '%' . $name . '%'))
+            ->when($this->columnSearch['email'] ?? null, fn ($query, $email) => $query->where('users.email', 'like', '%' . $email . '%'));
+    }
+
+    public function bulkActions(): array
+    {
+        return [
+            'activate' => 'Activate',
+            'deactivate' => 'Deactivate',
+        ];
+    }
+
+    public function activate()
+    {
+        User::whereIn('id', $this->getSelected())->update(['active' => true]);
+
+        $this->clearSelected();
+    }
+
+    public function deactivate()
+    {
+        User::whereIn('id', $this->getSelected())->update(['active' => false]);
+
+        $this->clearSelected();
+    }
+
+    public function reorder($items): void
+    {
+        foreach ($items as $item) {
+            User::find($item[$this->getPrimaryKey()])->update(['sort' => (int)$item[$this->getDefaultReorderColumn()]]);
+        }
     }
 }
